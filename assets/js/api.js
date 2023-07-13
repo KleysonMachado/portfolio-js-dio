829e5f64f90625f355525f82963b823334644105
KleysonMachado <kleysonm@gmail.com>
2023-07-13 18:08:47 -0300

    Terceiro commit

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://github.com/KleysonMachado/portfolio-js-dio/tree/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/KleysonMachado/portfolio-js-dio/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
